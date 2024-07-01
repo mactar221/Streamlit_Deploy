fc07813202e37ac2418f0fed2567866faa7f562d
mactar221 <161718673+mactar221@users.noreply.github.com>
2024-07-01 12:34:21 +0000

    Update checkpoint_1.py

--- checkpoint_1.py
+++ checkpoint_1.py
@@ -178,7 +178,7 @@ import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
-df = pd.read_csv("sampled_df).csv")
+df = pd.read_csv("output).csv")
 pr = df.profile_report()
 
 st_profile_report(pr)
